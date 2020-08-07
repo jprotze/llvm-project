5f11929d436aefd3801905b55189cc160af6c9b7
Joachim Protze <protze@itc.rwth-aachen.de>
2020-08-07 14:02:33 +0200

    Drop reads completely

--- compiler-rt/lib/tsan/rtl/tsan_rtl.cpp
+++ compiler-rt/lib/tsan/rtl/tsan_rtl.cpp
@@ -718,13 +718,16 @@ void MemoryAccessImpl1_template(ThreadState *thr, uptr addr,
     return;
 
   // we only store read access if happened-before or sometime on same thread
-  old = LoadShadow(shadow_mem + (cur.epoch() % kShadowCnt));
   if (LIKELY(!kAccessIsWrite))
+    return;
+/*  old = LoadShadow(shadow_mem + (cur.epoch() % kShadowCnt));
+  if (LIKELY(!kAccessIsWrite)) {
     if (LIKELY(Shadow::TidsAreEqual(old, cur))) {
       if (LIKELY(cur.epoch() % 127 != 0))
         return;
     } else if(LIKELY(!HappensBefore(cur, thr)))
       return;
+  }*/
   // choose a random candidate slot and replace it
   StoreShadow(shadow_mem + (cur.epoch() % kShadowCnt), store_word);
   StatInc(thr, StatShadowReplace);
